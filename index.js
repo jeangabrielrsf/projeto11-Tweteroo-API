2b270305470249b24e3927312adeae7c1f1e9958
Jean Frusca <jeang96@gmail.com>
2022-08-28 21:28:58 -0300

    Bônus: mensagens do post alteradas de acordo com os requisitos.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express, { response } from "express";
+import cors from "cors";
 
 let userData = [
 	{
@@ -11,62 +12,33 @@ let userData = [
 let tweetData = [
 	{
 		username: "bobesponja",
+		avatar:
+			"https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
 		tweet: "1",
-	},
-	{
-		username: "bobesponja",
-		tweet: "2",
-	},
-	{
-		username: "bobesponja",
-		tweet: "3",
-	},
-	{
-		username: "bobesponja",
-		tweet: "4",
-	},
-	{
-		username: "bobesponja",
-		tweet: "5",
-	},
-	{
-		username: "bobesponja",
-		tweet: "6",
-	},
-	{
-		username: "bobesponja",
-		tweet: "7",
-	},
-	{
-		username: "bobesponja",
-		tweet: "8",
-	},
-	{
-		username: "bobesponja",
-		tweet: "9",
-	},
-	{
-		username: "bobesponja",
-		tweet: "10",
 	},
 ];
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.post("/sign-up", (request, response) => {
 	console.log("recebi o post da rota /sign-up");
 	console.log(request.body);
 	userData = [...userData, request.body];
 	console.log(userData);
-	response.status(201).send("OK");
+	response.sendStatus(201);
 });
 
 server.post("/tweets", (request, response) => {
 	console.log("recebi um post na rota /tweets");
+	const userAvatar = userData.find(
+		(item) => request.body.username === item.username
+	);
+	console.log(userAvatar);
 	tweetData = [...tweetData, request.body];
 	console.log(tweetData);
-	response.status(201).send("OK");
+	response.sendStatus(201);
 });
 
 server.get("/tweets", (request, response) => {
